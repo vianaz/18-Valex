547e3421402020cc323a762a3c3a58b6da7bab10
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-07-12 16:52:23 -0300

    changes

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,16 +2,9 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const databaseConfig: IConfigDB = {
+export const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
-};
-
-if (process.env.MODE === "PROD") {
-  databaseConfig.ssl = {
-    rejectUnauthorized: false,
-  };
-}
-
-const connection = new Pool(databaseConfig);
-
-export default connection;
+  ssl: {
+    rejectUnauthorized: process.env.PROD === "true",
+  },
+});
